a28aad2a065306a87257989a2ed1ee37f4b2143f
Otavio Astrath Leite <otavioastrath00@gmail.com>
2024-03-26 14:18:23 +0100

    printed first time

---
+++ testing/testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Otavio Astrath Leite on 26.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        printContent("Hello World")
+    }
+
+
+}
+
